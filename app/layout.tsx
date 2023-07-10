65bea97a69dd4cc83e304667ef2204e109d35b23
Júlio César <juliobento237@gmail.com>
2023-07-10 14:01:36 -0300

    (setup): setup folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { robotoMono } from "@/config/font";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={robotoMono.className}>{children}</body>
+    </html>
+  );
+}
